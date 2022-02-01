624b31ef012631cb9b6159a9e7b84ef8fb7e573f
Ariel Bogdziewicz <ariel.bogdziewicz@gmail.com>
2022-02-01 21:51:07 +0100

    Initial Commit

---
+++ DetectStrings/DetectStrings.h
@@ -0,0 +1,18 @@
+//
+//  DetectStrings.h
+//  DetectStrings
+//
+//  Created by Ariel Bogdziewicz on 01/02/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DetectStrings.
+FOUNDATION_EXPORT double DetectStringsVersionNumber;
+
+//! Project version string for DetectStrings.
+FOUNDATION_EXPORT const unsigned char DetectStringsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DetectStrings/PublicHeader.h>
+
+
